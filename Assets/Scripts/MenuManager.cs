2fa712a3343ded78ec1ee1a156e79711fb45418c
Lloyd <Driven.lloyd@gmail.com>
2020-06-20 16:39:41 +0100

    Implemented UI Loop
    [Feature] Added in Main Menu scene
    
    [Feature] Added in Play, Instructions, Quit buttons to main menu
    
    [Feature] Added in pause menu to game scene
    
    [Update] Added ability to pause game
    
    [Feaure] Added in a MenuManager script that can be used across the menus
    
    Task 2: ~ 1h 40 minutes

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        if (PauseMenuUI != null)
+        {
+            PauseMenuUI.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+            //If not passed in pause menu, then we are in the main menu so quit
+            if (PauseMenuUI != null)
+            {
+                Pause();
+            }
+            else
+            {
+                QuitGame();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+
+        if (PauseMenuUI != null)
+        {
+            PauseMenuUI.SetActive(true);
+            //Set timescale to 0 to pause the game
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void Resume()
+    {
+
+        if (PauseMenuUI != null)
+        {
+            PauseMenuUI.SetActive(false);
+            //Set timescale to 1 to resume the game
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void LoadGame()
+    {
+        Time.timeScale = 1.0f;
+        //Load scene. For the time being I prefer to use name, however I believe it is better to use build index.
+        SceneManager.LoadScene("Game");
+    }
+
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        //Load scene. For the time being I prefer to use name, however I believe it is better to use build index.
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You have quit the game!");
+        Application.Quit();
+    }
+}
