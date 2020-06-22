68849e320e54072c7903c691dd1c21b963eb8f4d
Lloyd <Driven.lloyd@gmail.com>
2020-06-22 22:00:18 +0100

    Adding in push/pull mechanic
    [Feature] Added in panels that push and pull < Needs refining
    
    [Bug] Fixed messed up animation on hippo
    
    Note: panels should be forward vector, wanted working for now, will change
    them tomorrow
    
    Task 7 ~ 1.5 hours, Task 8 ~ 0.2h

--- Assets/Resources/Scripts/MarbleBehaviour.cs
+++ Assets/Resources/Scripts/MarbleBehaviour.cs
@@ -22,36 +22,37 @@ public class MarbleBehaviour : MonoBehaviour
     public Rigidbody marbleRigidbody;
     public bool resetMe = false;
     public bool endMe = false;
+    public MeshRenderer marbleMeshRenderer;
 
     //Setup the marble based on colour
     public void SetupMarble(MarbleColour Colour)
     {
-        MeshRenderer Mesh = this.GetComponent<MeshRenderer>();
+        marbleMeshRenderer = this.GetComponent<MeshRenderer>();
         marbleRigidbody = this.GetComponent<Rigidbody>();
         switch (Colour)
         {
             case MarbleColour.GREEN:
                 marbleValue = 10;
-                Mesh.material = Resources.Load("Materials/Green", typeof(Material)) as Material;
+                marbleMeshRenderer.material = Resources.Load("Materials/Green", typeof(Material)) as Material;
                 break;
             
             case MarbleColour.BLUE:
                 marbleValue = 20;
-                Mesh.material = Resources.Load("Materials/Blue", typeof(Material)) as Material;
+                marbleMeshRenderer.material = Resources.Load("Materials/Blue", typeof(Material)) as Material;
                 break;
 
             case MarbleColour.YELLOW:
-                Mesh.material = Resources.Load("Materials/Yellow", typeof(Material)) as Material;
+                marbleMeshRenderer.material = Resources.Load("Materials/Yellow", typeof(Material)) as Material;
                 marbleValue = 30;
                 break;
 
             case MarbleColour.PURPLE:
-                Mesh.material = Resources.Load("Materials/Purple", typeof(Material)) as Material;
+                marbleMeshRenderer.material = Resources.Load("Materials/Purple", typeof(Material)) as Material;
                 marbleValue = 50;
                 break;
 
             case MarbleColour.RED:
-                Mesh.material = Resources.Load("Materials/Red", typeof(Material)) as Material;
+                marbleMeshRenderer.material = Resources.Load("Materials/Red", typeof(Material)) as Material;
                 marbleValue = -60;
                 break;
           
@@ -79,6 +80,7 @@ public class MarbleBehaviour : MonoBehaviour
         {
             resetMe = true;
         }
+        Debug.DrawRay(transform.position, (transform.forward *2), Color.cyan);
     }
 
     private void OnCollisionStay(Collision collision)
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -9,6 +9,17 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     public GameObject scoreUI;
     Text uiScoreText;
+    int wallAmount;
+    int wallHighlight;
+    float highlightChangeCountdown;
+    public GameObject[] wallPanels;
+    public Material arenaMaterial;
+    public Material highlightMaterial;
+    bool highligthReverse = false;
+    public float force = 0;
+    float forceCap = 2.0f;
+    float forceAmplifier= 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +29,48 @@ public class PlayerController : MonoBehaviour
         {
             uiScoreText = scoreUI.GetComponent<Text>();
         }
+        wallAmount = wallPanels.Length;
+        highlightChangeCountdown = 0.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        highlightChangeCountdown -= Time.deltaTime;
+
+        if(highlightChangeCountdown < 0)
+        {
+            wallPanels[wallHighlight].GetComponent<MeshRenderer>().material = arenaMaterial;
+            if (!highligthReverse)
+            {
+                wallHighlight++;
+            }
+            else
+            {
+                wallHighlight--;
+            }
+            if (wallHighlight == wallAmount-1 )
+            {
+                highligthReverse = true;
+            }
+            if (wallHighlight == 0)
+            {
+                highligthReverse = false ;
+            }
+            wallPanels[wallHighlight].GetComponent<MeshRenderer>().material = highlightMaterial;
+            highlightChangeCountdown = 0.2f;
+        }
+        RaycastHit objectHit;
+        Debug.DrawRay(wallPanels[wallHighlight].transform.position, -wallPanels[wallHighlight].transform.right * 30, Color.green);
+        if (Physics.Raycast(wallPanels[wallHighlight].transform.position, -wallPanels[wallHighlight].transform.right, out objectHit, 30))
+        {
+            if (objectHit.collider.gameObject.tag == "Marble")
+            {
+                Debug.Log("Ray hit marble");
+                objectHit.rigidbody.AddForce(-wallPanels[wallHighlight].transform.right * force, ForceMode.VelocityChange);
+
+            }
+        }
         Debug.Log(score);
         //play the neck animation
         if(Input.GetKey(KeyCode.Space))
@@ -33,8 +81,20 @@ public class PlayerController : MonoBehaviour
                 animator.Play("Base Layer.NeckAnim", 0, 0);
             }
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            force = forceCap;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            force = -forceCap;
+        }
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W))
+        {
+            force = 0;
+        }
         //Change UI Score
-        if(scoreUI)
+        if (scoreUI)
         {
             uiScoreText.text = score.ToString();
         }
@@ -48,11 +108,13 @@ public class PlayerController : MonoBehaviour
         behaviour.endMe = true;
 
     }
+    
     public void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Marble" && Input.GetKey(KeyCode.Space))
         {
             Debug.Log(other.gameObject.name);
+            other.attachedRigidbody.velocity = Vector3.zero;
             StartCoroutine(EatMarble(other.gameObject));
         }
     }
